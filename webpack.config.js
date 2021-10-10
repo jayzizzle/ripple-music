727a4e35fdc118990b67fc4debc4b1c0d638c6a4
jayzizzle <dj.jonzamora@gmail.com>
2021-10-09 19:41:15 -0700

    add frontend

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: './frontend/ripple.jsx',
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: 'defaults' }],
+                            ['@babel/react', { targets: 'defaults' }],
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: ['.js', '.jsx', '*'],
+    },
+};
