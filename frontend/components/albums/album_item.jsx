cba99b716afd983b6180596d69b0bc6cc71faac9
jayzizzle <dj.jonzamora@gmail.com>
2021-10-20 22:46:49 -0700

    add styling for album items

--- frontend/components/albums/album_item.jsx
+++ frontend/components/albums/album_item.jsx
@@ -4,18 +4,13 @@ import AlbumHeartContainer from '../heart_button/album_heart_container';
 export const AlbumItem = (props) => {
   const { album } = props;
   return (
-    <li>
-      <img
-        className='album'
-        width='150px'
-        height='150px'
-        src={album.coverUrl}
-      />
+    <li className='flex-col-start album-li'>
+      <img className='cover' src={album.coverUrl} />
       <h4>{album.title}</h4>
-      <p>{album.artistName}</p>
-      <p>{album.category}</p>
-      <p>
-        {album.year}&nbsp;{album.isExplicit ? 'Explicit' : null}
+      <p className='album-artist'>{album.artistName}</p>
+      <p className='album-sm-caps'>
+        {album.year}&nbsp;
+        {album.isExplicit ? <span class='explicit'>Explicit</span> : null}
       </p>
       <AlbumHeartContainer
         itemId={album.id}
--- frontend/components/albums/albums.jsx
+++ frontend/components/albums/albums.jsx
@@ -12,7 +12,7 @@ export const Albums = (props) => {
     <div className='flex-row-start'>
       <main className='flex-col-start'>
         <h1 className='section-title'>My Albums</h1>
-        <ul>
+        <ul className='flex-row-start flex-wrap'>
           {props.likedAlbums.map((album) => (
             <AlbumItem album={album} likes={props.likes} key={album.id} />
           ))}
