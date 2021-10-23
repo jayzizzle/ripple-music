131ad2708e0d9571e974ce77181d6687cd0658fe
jayzizzle <dj.jonzamora@gmail.com>
2021-10-22 18:45:08 -0700

    add track like api util

--- frontend/util/like_api_util.js
+++ frontend/util/like_api_util.js
@@ -1,3 +1,5 @@
+// ALBUM LIKES
+
 export const getAllAlbumLikes = (userId) => {
   return $.ajax({
     method: 'GET',
@@ -24,5 +26,36 @@ export const deleteAlbumLike = (userId, albumLikeId) => {
   return $.ajax({
     method: 'DELETE',
     url: `/api/users/${userId}/album_likes/${albumLikeId}`,
+  });
+};
+
+// TRACK LIKES
+
+export const getAllTrackLikes = (userId) => {
+  return $.ajax({
+    method: 'GET',
+    url: `/api/users/${userId}/track_likes`,
+  });
+};
+
+export const getTrackLike = (userId, trackLikeId) => {
+  return $.ajax({
+    method: 'GET',
+    url: `/api/users/${userId}/track_likes/${trackLikeId}`,
+  });
+};
+
+export const postTrackLike = (userId, trackLike) => {
+  return $.ajax({
+    method: 'POST',
+    url: `/api/users/${userId}/track_likes`,
+    data: { trackLike },
+  });
+};
+
+export const deleteTrackLike = (userId, trackLikeId) => {
+  return $.ajax({
+    method: 'DELETE',
+    url: `/api/users/${userId}/track_likes/${trackLikeId}`,
   });
 };
