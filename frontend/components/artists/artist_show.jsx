7f7b40512691d06f0b684d66a0b53fcae045a112
jayzizzle <dj.jonzamora@gmail.com>
2021-10-24 15:07:24 -0700

    add background image to artist show pages

--- frontend/components/artists/artist_show.jsx
+++ frontend/components/artists/artist_show.jsx
@@ -10,18 +10,30 @@ export const ArtistShow = (props) => {
   if (!props.artist || !props.albums) return null;
 
   return (
-    <div className='flex-row-start'>
+    <div className='flex-row-start black-bg'>
       <main className='flex-col-start'>
-        <h1 className='section-title'>{props.artist.artistName}</h1>
-        <div className='image-crop'>
-          <img className='artist-photo' src={props.artist.photoUrl} />
+        <div
+          className='artist-bg'
+          style={{
+            backgroundImage: `linear-gradient(transparent, #000 75%), url(${props.artist.photoUrl})`,
+          }}
+        ></div>
+        <div className='flex-col-start z1'>
+          <h1 className='section-title'>{props.artist.artistName}</h1>
+          <div className='image-crop'>
+            <img className='artist-photo' src={props.artist.photoUrl} />
+          </div>
+          <h3 className='subtitle'>Albums by {props.artist.artistName}</h3>
+          <ul className='flex-row-start flex-wrap'>
+            {props.albums.map((album) => (
+              <AlbumItem
+                album={album}
+                likes={props.likedAlbums}
+                key={album.id}
+              />
+            ))}
+          </ul>
         </div>
-        <h3 className='subtitle'>Albums by {props.artist.artistName}</h3>
-        <ul className='flex-row-start flex-wrap'>
-          {props.albums.map((album) => (
-            <AlbumItem album={album} likes={props.likedAlbums} key={album.id} />
-          ))}
-        </ul>
       </main>
     </div>
   );
