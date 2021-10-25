a9b3febf3f489d03583fa68e004c327e85294fa8
jayzizzle <dj.jonzamora@gmail.com>
2021-10-24 20:44:09 -0700

    add playlist and playlist track models

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,2 +1,22 @@
 class Playlist < ApplicationRecord
+
+  validates :title, :user_id, presence: true
+  validates :user_id, uniqueness: { scope: :title }
+
+  belongs_to :user,
+    foreign_key: :user_id,
+    class_name: 'User'
+
+  has_many :playlist_tracks,
+    foreign_key: :playlist_id,
+    class_name: 'PlaylistTrack'
+
+  has_many :artists,
+    through: :playlist_tracks,
+    source: :artist
+
+  has_many :tracks,
+    through: :playlist_tracks,
+    source: :track
+
 end
--- app/models/playlist_track.rb
+++ app/models/playlist_track.rb
@@ -1,2 +1,14 @@
 class PlaylistTrack < ApplicationRecord
+
+  validates :playlist_id, :track_id, presence: true
+  validates :playlist_id, uniqueness: { scope: :track_id }
+
+  belongs_to :playlist,
+    foreign_key: :playlist_id,
+    class_name: 'Playlist'
+
+  belongs_to :track,
+    foreign_key: :track_id,
+    class_name: 'Track'
+
 end
