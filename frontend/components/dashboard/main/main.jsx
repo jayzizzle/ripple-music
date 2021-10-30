29a0ddf7520352efbfbf429f6f41b50eba6837cb
jayzizzle <dj.jonzamora@gmail.com>
2021-10-30 10:16:59 -0700

    add random features to main page

--- frontend/components/dashboard/main/main.jsx
+++ frontend/components/dashboard/main/main.jsx
@@ -1,7 +1,25 @@
 import React, { useEffect } from 'react';
+import { AlbumItem } from '../../albums/album_item';
+import { ArtistItem } from '../../artists/artist_item';
+import { shuffle } from '../../../util/helper_util';
 
 export const Main = (props) => {
-  if (!props.user.username) return null;
+  let featuredAlbums;
+  let featuredArtists;
+
+  useEffect(() => {
+    props.getAllFeatures();
+    return function cleanup() {
+      props.clearAlbums();
+    };
+  }, []);
+
+  if (!props.user.username || !props.albums || !props.artists) {
+    return null;
+  } else {
+    featuredAlbums = shuffle(props.albums);
+    featuredArtists = shuffle(props.artists);
+  }
 
   return (
     <div className='flex-row-start black-bg'>
@@ -11,6 +29,27 @@ export const Main = (props) => {
           <div className='flex-row-between full-width artist-heading'>
             <h1 className='artist-h1'>Welcome back, {props.user.username}!</h1>
           </div>
+          <h1 className='section-title'>Albums</h1>
+          <ul className='flex-row-start flex-wrap'>
+            {featuredAlbums.map((album) => (
+              <AlbumItem
+                album={album}
+                likes={props.albumLikes}
+                key={album.id}
+              />
+            ))}
+          </ul>
+          <div className='break'></div>
+          <h1 className='section-title'>Artists</h1>
+          <ul className='flex-row-start flex-wrap'>
+            {featuredArtists.map((artist) => (
+              <ArtistItem
+                artist={artist}
+                follows={props.follows}
+                key={artist.id}
+              />
+            ))}
+          </ul>
         </div>
       </main>
     </div>
