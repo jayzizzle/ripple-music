6a7aff6c91a80ec6b812cf9095e49fe06e545dbd
jayzizzle <dj.jonzamora@gmail.com>
2021-11-01 19:20:54 -0700

    clean up test and test container

--- frontend/components/dashboard/player/test_container.js
+++ frontend/components/dashboard/player/test_container.js
@@ -1,19 +1,15 @@
 import { connect } from 'react-redux';
 import { Test } from './test';
-import { getAlbum } from '../../../actions/album_actions';
 import { setToPlay, setToPause } from '../../../actions/ui_actions';
 import { receivedCurrentTrack } from '../../../actions/current_playlist_actions';
 
 const mSTP = (state) => ({
   currentPlaylist: Object.values(state.entities.currentPlaylist),
-  albums: state.entities.albums,
-  tracks: Object.values(state.entities.tracks),
   isCurrentlyPlaying: state.ui.isPlaying,
   currentSongIndex: state.entities.currentTrack.idx,
 });
 
 const mDTP = (dispatch) => ({
-  getAlbum: (albumId) => dispatch(getAlbum(albumId)),
   setToPlay: (playerStatus) => dispatch(setToPlay(playerStatus)),
   setToPause: (playerStatus) => dispatch(setToPause(playerStatus)),
   receivedCurrentTrack: (currentTrack) =>
